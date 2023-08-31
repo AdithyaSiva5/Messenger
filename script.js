d48c74aff70b3aa8a4191bba8709beca0853a998
AdithyaSiva5 <142294963+AdithyaSiva5@users.noreply.github.com>
2023-08-31 16:11:04 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,37 @@
+
+const signInForm = document.getElementById("signin-form")
+
+signInForm.addEventListener("submit", valid);
+function valid() {
+
+    let password = document.getElementById("password").value;
+    let userInfo = document.getElementById("userInfo").value;
+    let userError = document.getElementById("user-error")
+    let passwordError = document.getElementById("password-error")
+    if(userInfo=="")
+    {
+        userError.innerHTML="number or email please"
+    }else if(!isNaN(userInfo))
+    {
+        if(userInfo<Math.pow(10,9))
+        userError.innerHTML="Enter 10 digit of numbers"
+    }
+    else if(!userInfo.includes("@"))
+    {
+            userError.innerHTML="Enter a valid email"
+    }
+    else{
+        userError.innerHTML=""
+    }
+
+    if(password=="")
+    {
+        passwordError.innerHTML="Why can't you type the password"
+        
+    }else if (password.length<6) {
+        passwordError.innerHTML="type more..."
+    }
+    else{
+        passwordError.innerHTML="";
+    }
+}
